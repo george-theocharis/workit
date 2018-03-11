0c6c365004d55f372f0889fed6c778a334c1d21e
DESKTOP-HO258IH\George Theocharis <giwrgos.theocharis@gmail.com>
2018-03-11 16:32:35 +0200

    no message

---
+++ Workit/app/src/main/java/team/gap/workit/views/Login/LoginView.kt
@@ -0,0 +1,12 @@
+package team.gap.workit.views.Login
+
+import com.hannesdorfmann.mosby3.mvp.MvpView
+
+/**
+ * Created by George Theocharis on 3/11/2018.
+ */
+
+interface LoginView : MvpView
+{
+    
+}
