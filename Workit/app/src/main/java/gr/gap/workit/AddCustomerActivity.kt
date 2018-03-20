0f465f54c792f7b174d95ef1542a97e4af47f2ec
giorgosTh <gt@anaxoft.com>
2018-03-20 15:03:25 +0200

    no message

---
+++ Workit/app/src/main/java/gr/gap/workit/AddCustomerActivity.kt
@@ -0,0 +1,12 @@
+package gr.gap.workit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCustomerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_customer)
+    }
+}
