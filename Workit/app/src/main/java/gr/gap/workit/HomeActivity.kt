f67dd5b1ed0e6532d4011c24f6aceee3fc4f4334
giorgosTh <gt@anaxoft.com>
2018-03-20 14:40:39 +0200

    no message

--- Workit/app/src/main/java/gr/gap/workit/HomeActivity.kt
+++ Workit/app/src/main/java/gr/gap/workit/HomeActivity.kt
@@ -2,11 +2,20 @@ package gr.gap.workit
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
