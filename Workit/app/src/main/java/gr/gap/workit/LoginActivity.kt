aac52f2902226ab0c555594c1ea8c01ca64ae849
JonnyPap <jonny.papageorgiou@gmail.com>
2018-03-21 11:41:14 +0200

    forgot password popup

--- Workit/app/src/main/java/gr/gap/workit/LoginActivity.kt
+++ Workit/app/src/main/java/gr/gap/workit/LoginActivity.kt
@@ -1,8 +1,15 @@
 package gr.gap.workit
 
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
+import android.support.v7.app.AlertDialog
+import android.text.InputType
+import android.widget.EditText
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -12,6 +19,8 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         textViewRegister.setOnClickListener { navigateToRegister() }
+
+        textView.setOnClickListener{showForgotPasswordDialog()}
 
         btn_login.setOnClickListener { navigateToHome()}
     }
@@ -24,5 +33,25 @@ class LoginActivity : AppCompatActivity() {
     private fun navigateToHome() {
         val intent = Intent(this, HomeActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun showForgotPasswordDialog() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        lateinit var editTextMail: EditText
+
+        with (dialogBuilder){
+            setTitle(R.string.forgotPasswordTitle)
+            setMessage(R.string.forgotPasswordMessage)
+            editTextMail = EditText(this.context)
+            editTextMail?.hint= getString(R.string.email)
+            editTextMail?.inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+            setPositiveButton(R.string.send){
+                dialog, _ -> dialog.dismiss()
+            }
+        }
+
+        val dialog = dialogBuilder.create()
+        dialog.setView(editTextMail)
+        dialog.show()
     }
 }
