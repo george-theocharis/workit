e9ba06b0c681ca4055fcedd2f96c7a2de183b131
giorgosTh <gt@anaxoft.com>
2018-04-16 10:52:01 +0300

    no message

--- Workit/app/src/main/java/gr/gap/workit/data/di/app/AppComponent.kt
+++ Workit/app/src/main/java/gr/gap/workit/data/di/app/AppComponent.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import dagger.BindsInstance
 import dagger.Component
 
-@Component(modules = arrayOf(AppModule::class))
+@Component(modules = [(AppModule::class)])
 interface AppComponent {
     @Component.Builder
     interface Builder {
--- Workit/app/src/main/java/gr/gap/workit/data/di/login/LoginComponent.kt
+++ Workit/app/src/main/java/gr/gap/workit/data/di/login/LoginComponent.kt
@@ -1,12 +1,9 @@
 package gr.gap.workit.data.di.login
 
 import dagger.Component
-import gr.gap.workit.data.di.app.AppComponent
-import gr.gap.workit.domain.usecases.LoginUserUseCase
 import gr.gap.workit.presentation.LoginView.LoginPresenter
 
 @Component(modules = [(LoginModule::class)])
 interface LoginComponent {
     fun presenter(): LoginPresenter
-    fun loginUserUseCase(): LoginUserUseCase
 }
