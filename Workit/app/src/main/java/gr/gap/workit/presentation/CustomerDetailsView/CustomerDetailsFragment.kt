d380ea47d67c704a225c8b4598ce255fc0fff623
giorgosTh <gt@anaxoft.com>
2018-04-19 16:37:58 +0300

    no message

--- Workit/app/src/main/java/gr/gap/workit/presentation/CustomerDetailsView/CustomerDetailsFragment.kt
+++ Workit/app/src/main/java/gr/gap/workit/presentation/CustomerDetailsView/CustomerDetailsFragment.kt
@@ -1,13 +1,16 @@
 package gr.gap.workit.presentation.CustomerDetailsView
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import com.hannesdorfmann.mosby3.mvi.MviFragment
 import gr.gap.workit.R
 import gr.gap.workit.data.di.App
 import io.reactivex.Observable
+import kotlinx.android.synthetic.main.fragment_customer_details.*
 
 class CustomerDetailsFragment : MviFragment<CustomerDetailsView, CustomerDetailsPresenter>(), CustomerDetailsView {
 
@@ -15,6 +18,13 @@ class CustomerDetailsFragment : MviFragment<CustomerDetailsView, CustomerDetails
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_customer_details,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        phonesList.layoutManager = LinearLayoutManager(context, LinearLayout.VERTICAL, false)
+        locationsList.layoutManager = LinearLayoutManager(context, LinearLayout.VERTICAL, false)
     }
 
     override fun loadCustomerDetailsIntent(): Observable<Boolean> = Observable.just(true)
@@ -33,6 +43,11 @@ class CustomerDetailsFragment : MviFragment<CustomerDetailsView, CustomerDetails
 
     private fun renderData(state: CustomerDetailsViewState.Data){
         (context as CustomerDetailsActivity)?.UpdateHeader(state.customer)
+
+        phonesList.adapter = CustomerPhonesAdapter(state.customer.phones!!)
+        locationsList.adapter = CustomerAddressesAdapter(state.customer.addresses!!)
+
+        emailText.text = state.customer.email
     }
 
     private fun renderError(state: CustomerDetailsViewState.Error){
