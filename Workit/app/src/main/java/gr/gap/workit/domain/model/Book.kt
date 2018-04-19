ec0b0addfec143fac2cc61bd8bb62f253e2fff47
giorgosTh <gt@anaxoft.com>
2018-04-19 13:13:14 +0300

    no message

--- Workit/app/src/main/java/gr/gap/workit/domain/model/Book.kt
+++ Workit/app/src/main/java/gr/gap/workit/domain/model/Book.kt
@@ -1,3 +1,5 @@
 package gr.gap.workit.domain.model
 
-data class Book(val id: Int?)
+import com.squareup.moshi.Json
+
+data class Book(val id: Int?, val name: String?, @Json(name="created_at") val createdAt: String?)
