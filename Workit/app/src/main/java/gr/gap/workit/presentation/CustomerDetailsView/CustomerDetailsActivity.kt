4437eaf810d00a583ba487f2c3a1441c26ef37b1
giorgosTh <gt@anaxoft.com>
2018-04-25 14:22:26 +0300

    animate fragments

--- Workit/app/src/main/java/gr/gap/workit/presentation/CustomerDetailsView/CustomerDetailsActivity.kt
+++ Workit/app/src/main/java/gr/gap/workit/presentation/CustomerDetailsView/CustomerDetailsActivity.kt
@@ -48,7 +48,9 @@ class CustomerDetailsActivity : AppCompatActivity() {
         }
 
         supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, fragment, tag).commit()
+                .setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out)
+                .replace(R.id.fragment_container, fragment, tag)
+                .commit()
 
         return true
     }
