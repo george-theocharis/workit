db48ab85af94fa87a2b3de850cc52fcacdd1c138
JonnyPap <jonny.papageorgiou@gmail.com>
2018-05-15 11:34:08 +0300

    no message

--- Workit/app/src/main/java/gr/gap/workit/presentation/LoginView/LoginActivity.kt
+++ Workit/app/src/main/java/gr/gap/workit/presentation/LoginView/LoginActivity.kt
@@ -2,9 +2,11 @@ package gr.gap.workit.presentation.LoginView
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.transition.TransitionManager
 import android.support.v7.app.AlertDialog
 import android.text.InputType
 import android.view.View
+import android.view.ViewGroup
 import android.widget.EditText
 import com.hannesdorfmann.mosby3.mvi.MviActivity
 import com.jakewharton.rxbinding2.view.RxView
@@ -13,6 +15,7 @@ import gr.gap.workit.data.di.App
 import gr.gap.workit.presentation.HomeView.HomeActivity
 import gr.gap.workit.presentation.RegisterView.RegisterActivity
 import io.reactivex.Observable
+import kotlinx.android.synthetic.main.activity_customer_details.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : MviActivity<LoginView, LoginPresenter>(), LoginView {
@@ -73,17 +76,17 @@ class LoginActivity : MviActivity<LoginView, LoginPresenter>(), LoginView {
     }
 
     private fun renderLoading(){
-        layoutEmail.visibility = View.GONE
+        TransitionManager.beginDelayedTransition(login_content as ViewGroup)
+        content.visibility = View.GONE
+        loginProgressBar.visibility = View.VISIBLE
     }
 
     private fun renderLogin(state: LoginViewState){
-        layoutEmail.visibility = View.VISIBLE
         navigateToHome()
         finish()
     }
 
     private fun renderError(state: LoginViewState){
-        layoutEmail.visibility = View.VISIBLE
         textLogo.text = (state as LoginViewState.Error).error.message
     }
 }
